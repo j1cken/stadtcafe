bf0b0293a637ab83214f5c6464aab847daca1ce0
Torben Jaeger <torben@jit-central.com>
2014-04-28 21:43:13 +0200

    changed email adress

--- php/index.php
+++ php/index.php
@@ -132,7 +132,7 @@
           <h2>Cafe Helmbundhöhe</h2>
           <p>Helmbundstrasse 20 | 74196 Neuenstadt</p>
           <p>T +49 7139 512 92 85 | Fax +49 7139 512 92 87</p>
-          <p><a class="btn btn-danger" href="mailto:jaeger.tanja@gmail.com?Subject=Anfrage%20Website" role="button">Email &raquo;</a></p>
+          <p><a class="btn btn-danger" href="mailto:tanja@cafe-helmbundhoehe.de?Subject=Anfrage%20Website" role="button">Email &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 
