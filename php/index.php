5d2b69dbd357f2a6369425ba8f9e1c91d5b773b1
Torben Jaeger <torben@jit-central.com>
2014-09-09 12:00:14 +0200

    removed google maps from carousel

--- php/index.php
+++ php/index.php
@@ -80,7 +80,6 @@
 	      <img src="images/info.png" alt="Info"/>
 	      <h1>Motorman Run 2014</h1>
 	      <p>Wir sind am Start. Mit einem Verpflegungsstand.</p>
-	      <p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2605.1212678280126!2d9.329550000000001!3d49.23619!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47982354430e217b%3A0x2846f535af9d2b98!2sHelmbundstra%C3%9Fe+20%2C+74196+Neuenstadt+am+Kocher!5e0!3m2!1sde!2sde!4v1410255978268" width="400" height="300" frameborder="0" style="border:0"></iframe></p>
 	    </div>
 	  </div>
 	</div>
