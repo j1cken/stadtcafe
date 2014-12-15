75af3d46a21c271d127f1b4d49c2234f33a50b02
Torben Jaeger <torben@jit-central.com>
2014-12-15 13:46:59 +0100

    fixed typo

--- php/index.php
+++ php/index.php
@@ -104,7 +104,7 @@
                 <div class="col-md-4"><img src="images/info.png" alt="Info"/></div>
                 <div class="col-md-8">
                   <h1>Neue Öffnungszeiten 2015!</h1>
-		  <p>Ab 07. Januar 2015 sind wie folgt für euch da:</p>
+		  <p>Ab 07. Januar 2015 sind wir wie folgt für euch da:</p>
 		  <p>Mittwoch - Samstag von 09:00h bis 18:00h</p>
 		  <p style="color: red">Sonntags <b>von 12:00h</b> bis 18:00h</p>
                 </div>
